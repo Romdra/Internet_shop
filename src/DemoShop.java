b418e46eb7685fdc477da08fb9de7e982ec54bba
Roman <roman_dra@mail.ru>
2022-07-12 17:52:55 +0500

    add Authentication

---
+++ src/DemoShop.java
@@ -0,0 +1,11 @@
+public class DemoShop {
+
+    public static void main(String[] args) {
+
+        Authentication myProfile = new Authentication();
+
+
+        myProfile.inputLog();
+        myProfile.inputPassword();
+    }
+}
